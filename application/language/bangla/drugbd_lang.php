f7521e48119c7cd0b08cf5138a69e0dc979ff629
Yasir Taher <yasirtaher@yahoo.com>
2016-01-12 18:52:24 +0600

    languages added

---
+++ application/language/bangla/drugbd_lang.php
@@ -0,0 +1,228 @@
+<?php
+
+$lang['error_email_missing'] = 'ইমেইল সাবমিট করুন';
+$lang['error_url_missing'] = 'You must submit a URL';
+$lang['error_username_missing'] = 'You must submit a username';
+$lang['currency'] = '৳';
+
+/*dashboard manue*/
+$lang['dashboard'] = 'ড্যাশবোর্ড';;
+$lang['users'] = 'গ্রাহক';
+$lang['user_list'] = 'গ্রাহক তালিকা';
+$lang['add_user'] = 'নতুন  গ্রাহক যুক্ত করুন';
+$lang['products'] = 'পণ্য';
+$lang['product_list'] = 'পণ্য তালিকা ';
+$lang['add_product'] = 'নতুন পণ্য যুক্ত করুন ';
+$lang['category'] = 'ক্যাটাগরি';
+$lang['tags'] = 'ট্যাগ';
+$lang['email_templates'] = 'ইমেল টেমপ্লেট';
+$lang['shop_order'] = 'গ্রাহকের কেনাকাটা';
+$lang['delivery'] = 'ডেলিভারি';
+
+/*dashboard home*/
+$lang['pending_order_list'] = 'অপেক্ষমাণ কেনার তালিকা';
+$lang['phone'] = 'ফোন';
+$lang['date'] = 'তারিখ';
+$lang['status'] = 'স্ট্যাটাস';
+$lang['latest_product'] = 'নতুন পণ্য ';
+$lang['price'] = 'মূল্য';
+$lang['dsply_status'] = 'অপেক্ষমাণ';
+
+/*user list*/
+$lang['dtname'] = 'নাম';
+$lang['dtemail'] = 'ইমেইল';
+$lang['dtjoin_date'] = 'যুক্ত হওয়ার তারিখ';
+$lang['dtrole'] = 'ভূমিকা';
+$lang['dtview'] = 'বিস্তারিত';
+$lang['dtstatus'] = 'স্ট্যাটাস';
+$lang['dtshow'] = 'দেখুন';
+$lang['dtshow'] = 'লিপিভুক্ত বিষয়';
+$lang['dtsearch'] = 'অনুসন্ধান';
+
+/*add product*/
+$lang['trade_name'] = 'ট্রেড নাম';
+$lang['generic_name'] = 'জেনেরিক নাম ';
+$lang['add_category'] = 'ক্যাটাগরি যুক্ত  করুন ';
+$lang['add_tag'] = 'ট্যাগ যুক্ত করুন ';
+$lang['in_stock'] = 'স্টকে আছে  কি ?';
+$lang['order_always'] = 'স্টকে না থাকলেও অর্ডার নিন ';
+$lang['take_times'] = 'এক পাতা /বোতল  ঔষধ সরবচ্চ সেবন';
+$lang['manage_stock'] = 'স্টক নিয়ন্ত্রন করুন';
+$lang['quantity']="পরিমাণ";
+$lang['regular_price'] = 'নিয়মিত মূল্য ';
+$lang['sale_price'] = 'বিক্রয় মূল্য ';
+$lang['sale_start'] = 'বিক্রয় শুরুর তারিখ';
+$lang['sale_end'] = 'বিক্রয় শেষ তারিখ';
+$lang['product_desc'] = 'বর্ণনা';
+$lang['small_desc'] = 'সংক্ষিপ্ত বর্ণনা ';
+
+/*user information page*/
+$lang['user_information'] = 'গ্রাহকের তথ্য ';
+
+/*tag list page*/
+$lang['tag_list'] = 'ট্যাগ তালিকা';
+$lang['add_new_tag'] = 'নতুন ট্যাগ যুক্ত করুন';
+$lang['edit_tag'] = 'ট্যাগ সম্পাদন করুন';
+
+/*category page*/
+$lang['change_status'] = 'স্ট্যাটাস পরিবর্তন করুন';
+$lang['yes'] = 'হাঁ';
+$lang['no'] = 'না';
+$lang['edit_category_modal'] = 'ক্যাটাগরি সম্পাদন করুন ';
+$lang['sure_unpublish'] = 'আপনি কি প্রত্যাহার করতে চান ?';
+$lang['sure_publish'] = 'আপনি কি প্রকাশ করতে চান ?';
+
+/*email tem*/
+$lang['email_temp_list'] = 'ইমেল টেম্পলেট তালিকা';
+$lang['add_new_temp'] = 'নতুন ইমেল যুক্ত করুন';
+$lang['edit_email_temp'] = 'ইমেল টেম্পলেট সম্পাদন করুন';
+$lang['delete_email_temp'] = 'ইমেল টেম্পলেট মুছুন';
+$lang['con_temp_delete'] = 'আপনি কি মুছতে চান ?';
+$lang['select_email_temp'] = 'দয়া করে ইমেল টেম্পলেট নির্বাচন করুন ';
+$lang['select_user'] = 'দয়া করে ইউজার নির্বাচন করুন';
+$lang['successfully_send_mail'] = 'সফল ভাবে ইমাল পাঠানো হয়েছে';
+$lang['please_admin_rep'] = 'সফল ভাবে ইমাল পাঠানো হয়েছে দয়া করে উত্তরের জন্য অপেক্ষা করুন ';
+
+/*order page*/
+$lang['order_list'] = 'কেনাকাটার তালিকা';
+$lang['order_date'] = 'কেনাকাটা';
+$lang['delivery_date'] = 'ডেলিভারি';
+$lang['delivered_by'] = 'Delivered by';
+$lang['details'] = 'বিস্তারিত';
+
+/*single order*/
+$lang['finish_date'] = 'ঔষধ শেষ হবে ';
+
+/*email template*/
+$lang['deleted_successfully']='সফল ভাবে মুছে ফেলা হয়েছে ';
+//Home page
+$lang['tagline'] = 'বাংলাদেশের সর্ব প্রথম অনলাইন ফার্মেসিতে আপনাকে স্বাগতম';
+$lang['feature_product']='নির্বাচিত পণ্য';
+$lang['latest_product']='নতুন পণ্য ';
+$lang['read_more']='বিস্তারিত';
+
+//signup - login
+$lang['full_name']='পূর্ণনাম';
+$lang['password']='পাসওয়ার্ড (ইংরেজিতে লিখুন)';
+$lang['confirm_password']='পাসওয়ার্ড  পুনরায় লিখুন(ইংরেজিতে লিখুন)';
+$lang['email']='ইমেইল (ইংরেজিতে লিখুন)';
+$lang['login']='লগইন';
+$lang['signup']='নতুন অ্যাকাউন্ট খুলুন';
+
+$lang['confirm'] ='কনফার্ম';
+$lang['forgot_password']='পাসওয়ার্ড ভুলে গেছেন?';
+$lang['create_account']='নতুন অ্যাকাউন্ট খুলুন';
+$lang['remember_me']='মনে রাখুন ';
+$lang['have_account']='অ্যাকাউন্ট আছে? ';
+$lang['or']='অথবা';
+$lang['reset_password']='পাসওয়ার্ড উদ্ধার';
+
+//header
+$lang['logout']='লগ আউট করুন';
+$lang['profile_details']='বিস্তারিত প্রোফাইল ';
+$lang['cart']='কার্ট';
+$lang['profile_edit']='প্রোফাইল সম্পাদন করুন';
+$lang['my_chart']='My Chart';
+$lang['search']='অনুসন্ধান করুন...';
+$lang['place_order'] ='অর্ডার করুন';
+$lang['all_product'] ='সকল পণ্য';
+$lang['my_chart']='চার্ট';
+
+
+
+//FOOTER
+$lang['about']='পরিচিতি';
+$lang['contact']='যোগাযোগ ';
+$lang['privacy']='গোপনীয়তা';
+$lang['term']='শর্তাবলী';
+$lang['help']='সহযোগিতা';
+
+//Profile - Edit - Update
+$lang['edit']='সম্পাদন';
+$lang['name']='নাম';
+$lang['emailaddress']='ইমেইল';
+$lang['phone']='ফোন';
+$lang['prescription_download']='প্রেসক্রিপশন ডাউনলোড করুন';
+$lang['thana']='থানা';
+$lang['district']='জেলা';
+$lang['address']='ঠিকানা';
+
+$lang['prescription']='প্রেসক্রিপশন';
+$lang['regular_drug']='নিয়মিত সেবনক্রিত ওষুধ ';
+$lang['drug_name']='ওষুধের নাম';
+$lang['per_day']='Name';
+$lang['delivery_info']='ডেলিভারি তথ্য';
+$lang['submit']='সাবমিট করুন ';
+
+//CART
+$lang['total']='মোট';
+$lang['image']='ছবি ';
+$lang['price']='মূল্য';
+$lang['quantity']='পরিমাণ';
+$lang['no_product']='আপনার কার্টে এখনো কোন পণ্য যুক্ত করা হয়নি';
+$lang['checkout']='চেকআউট';
+
+$lang['subtotal']='মোট';
+$lang['delivery_fee']='ডেলিভারি ফি ';
+$lang['grand_Total']='সর্বমোট';
+$lang['remove']='মুছুন';
+
+//Single page
+$lang['stock']='স্টকে আছে';
+
+
+
+//error msg jquery
+$lang['required'] = "এই ফিল্ডটি প্রয়োজন";;
+$lang['pcs'] = "টি";
+
+//Search result
+$lang['result_found']='ফলাফল';
+$lang['list']='তালিকা';
+
+
+$lang['invoice']='নাম্বার';
+$lang['jquery_select_box_error'] = "যে কোন এক্টি সঠিক মান নির্বাচন করুন";;
+$lang['select_box_start_value'] = "নির্বাচন করুন";
+
+
+//CONTROLLER
+// USER
+$lang['add_success']='সফলভাবে যুক্ত করা হয়েছে';
+$lang['edit_success']='সফল ভাবে পরিবর্তন করা হয়েছে';
+$lang['edit_error']='পরিবর্তন করতে ব্যর্থ হয়েছে ';
+$lang['add_error']='যুক্ত করতে ব্যর্থ  হয়েছে ';
+$lang['not_found']='পাওয়া যায়নি';
+
+$lang['order_product_success']='ধন্যবাদ!';
+$lang['upload_error']='আপলোড করতে ব্যর্থ হয়েছে !';
+$lang['order_error']='ডেলিভার করা হয়েছে';
+$lang['order_success']='আপডেট করা হয়েছে ';
+$lang['login_error']='ইমেইল অথবা পাসওয়ার্ড ভুল হয়েছে ';
+
+$lang['calender_lang'] = "bn-bd";
+$lang['company_name'] = "কোম্পানির নাম";;
+$lang['sitemap'] = "সাইট ম্যাপ";
+$lang['home'] = "প্রথম পাতা";
+$lang['contact_us'] = "কোন প্রশ্ন থাকলে যোগাযোগ করুন";
+$lang['subject'] = "বিষয়";
+$lang['email_send_error'] = "ইমেইল পাঠাতে সমস্যা";
+$lang['min_length'] = "কমপক্ষে {0} অক্ষর লিখুন.";
+$lang['max_length'] = "সর্বোচ্চ {0} অক্ষর লিখুন.";;
+$lang['add_to_cart'] = "কার্টে যোগ করুন";;
+
+$lang['order_invoice_number'] = "অর্ডার ইনভয়েস নাম্বার";
+$lang['delivered_by_me'] = "আমি পৌছিয়ে দিয়েছি";
+$lang['delivered_by'] = "পৌছিয়ে দিয়েছে";
+
+$lang['not_calculated_yet'] = "এখনো হিসেব হইনি";
+$lang['no_data_found'] = "তথ্য  পাওয়া যায়নি";
+$lang['add_new'] = "নতুন যুক্ত করুন";
+
+
+$lang['phone']="ফোন";
+$lang['phone_number']="০১৬১৫৫২২২২২";
+$lang['seasonal']="মৌসুমি পণ্য";
+$lang['popular']="জনপ্রিয় পণ্য";
+$lang['feature_cat']="নির্বাচিত ক্যাটাগরি";
+$lang['latest_offer']="নতুন অফার";
