c8c4bee72f41c6b5e17d2314a379550e075267ba
yasirtaher <yasirtaher@yahoo.com>
2016-01-22 22:01:56 +0600

    a

--- application/views/frontend/user/user_profile.php
+++ application/views/frontend/user/user_profile.php
@@ -184,18 +184,29 @@
                     </div>
                 </div>
                 <div class="col-md-6 thumbnail">
-                    <!--                    <img class="img-responsive" src="http://placehold.it/800x300" alt="">-->
-
                     <div class="food-item-img" style="background-image:url('https://c.zmtcdn.com/data/reviews_photos/cd9/72473df2b9a350eff8caba9a28609cd9_1451843318.jpg')">
                         <h4 class="pull-right info-tag rat">$24.99</h4>
                         <h4 class="info-tag"><a href="#" style="color: #FFF">Product Name</a>
                         </h4>
-
-                        <!--                        <p>See more snippets like these online store reviews at <a target="_blank" href="http://bootsnipp.com">Bootsnipp - http://bootsnipp.com</a>.</p>-->
-                        <!--                        <p>Want to make these reviews work? Check out-->
-                        <!--                            <strong><a href="http://maxoffsky.com/code-blog/laravel-shop-tutorial-1-building-a-review-system/">this building a review system tutorial</a>-->
-                        <!--                            </strong>over at maxoffsky.com!</p>-->
-                        <!--                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>-->
+                    </div>
+                    <div class="caption-full"></div>
+                    <div class="ratings">
+                        <p class="pull-right">3 reviews</p>
+                        <p>
+                            <span class="glyphicon glyphicon-star"></span>
+                            <span class="glyphicon glyphicon-star"></span>
+                            <span class="glyphicon glyphicon-star"></span>
+                            <span class="glyphicon glyphicon-star"></span>
+                            <span class="glyphicon glyphicon-star-empty"></span>
+                            4.0 stars
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-6 thumbnail">
+                    <div class="food-item-img" style="background-image:url('https://c.zmtcdn.com/data/reviews_photos/cd9/72473df2b9a350eff8caba9a28609cd9_1451843318.jpg')">
+                        <h4 class="pull-right info-tag rat">$24.99</h4>
+                        <h4 class="info-tag"><a href="#" style="color: #FFF">Product Name</a>
+                        </h4>
                     </div>
                     <div class="caption-full"></div>
                     <div class="ratings">
@@ -217,12 +228,6 @@
                         <h4 class="pull-right info-tag rat">$24.99</h4>
                         <h4 class="info-tag"><a href="#" style="color: #FFF">Product Name</a>
                         </h4>
-
-                        <!--                        <p>See more snippets like these online store reviews at <a target="_blank" href="http://bootsnipp.com">Bootsnipp - http://bootsnipp.com</a>.</p>-->
-                        <!--                        <p>Want to make these reviews work? Check out-->
-                        <!--                            <strong><a href="http://maxoffsky.com/code-blog/laravel-shop-tutorial-1-building-a-review-system/">this building a review system tutorial</a>-->
-                        <!--                            </strong>over at maxoffsky.com!</p>-->
-                        <!--                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>-->
                     </div>
                     <div class="caption-full"></div>
                     <div class="ratings">
@@ -238,18 +243,10 @@
                     </div>
                 </div>
                 <div class="col-md-6 thumbnail">
-                    <!--                    <img class="img-responsive" src="http://placehold.it/800x300" alt="">-->
-
                     <div class="food-item-img" style="background-image:url('https://c.zmtcdn.com/data/reviews_photos/cd9/72473df2b9a350eff8caba9a28609cd9_1451843318.jpg')">
                         <h4 class="pull-right info-tag rat">$24.99</h4>
                         <h4 class="info-tag"><a href="#" style="color: #FFF">Product Name</a>
                         </h4>
-
-                        <!--                        <p>See more snippets like these online store reviews at <a target="_blank" href="http://bootsnipp.com">Bootsnipp - http://bootsnipp.com</a>.</p>-->
-                        <!--                        <p>Want to make these reviews work? Check out-->
-                        <!--                            <strong><a href="http://maxoffsky.com/code-blog/laravel-shop-tutorial-1-building-a-review-system/">this building a review system tutorial</a>-->
-                        <!--                            </strong>over at maxoffsky.com!</p>-->
-                        <!--                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>-->
                     </div>
                     <div class="caption-full"></div>
                     <div class="ratings">
@@ -264,37 +261,6 @@
                         </p>
                     </div>
                 </div>
-                <div class="col-md-6 thumbnail">
-                    <!--                    <img class="img-responsive" src="http://placehold.it/800x300" alt="">-->
-
-                    <div class="food-item-img" style="background-image:url('https://c.zmtcdn.com/data/reviews_photos/cd9/72473df2b9a350eff8caba9a28609cd9_1451843318.jpg')">
-                        <h4 class="pull-right info-tag rat">$24.99</h4>
-                        <h4 class="info-tag"><a href="#" style="color: #FFF">Product Name</a>
-                        </h4>
-
-                        <!--                        <p>See more snippets like these online store reviews at <a target="_blank" href="http://bootsnipp.com">Bootsnipp - http://bootsnipp.com</a>.</p>-->
-                        <!--                        <p>Want to make these reviews work? Check out-->
-                        <!--                            <strong><a href="http://maxoffsky.com/code-blog/laravel-shop-tutorial-1-building-a-review-system/">this building a review system tutorial</a>-->
-                        <!--                            </strong>over at maxoffsky.com!</p>-->
-                        <!--                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>-->
-                    </div>
-                    <div class="caption-full"></div>
-                    <div class="ratings">
-                        <p class="pull-right">3 reviews</p>
-                        <p>
-                            <span class="glyphicon glyphicon-star"></span>
-                            <span class="glyphicon glyphicon-star"></span>
-                            <span class="glyphicon glyphicon-star"></span>
-                            <span class="glyphicon glyphicon-star"></span>
-                            <span class="glyphicon glyphicon-star-empty"></span>
-                            4.0 stars
-                        </p>
-                    </div>
-                </div>
-
-
-
-
             </div>
 <!--            <div class="col-md-3">-->
 <!--                <p class="lead">Top Coook</p>-->
